b1c8bed7c3e31a1d6c1103f0134449ce1ccdd0ce
Slava PLeshkov <mail@slavapleshkov.com>
2019-08-03 17:17:15 +0300

    Add task03

---
+++ sprint09/t03/mx_comparator.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+bool equal_nums(int a, int b) {
+    return a == b;
+}
+int mx_comparator(const int *arr, int size, int x, bool (*compare)(int, int)){
+    
+    for (int i = 0; i < size; i++) {
+        if(equal_nums(arr[i], x))
+            return i;
+    }
+        
+    return -1;
+}
