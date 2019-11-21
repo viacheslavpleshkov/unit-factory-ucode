a6aced11e50345c292c25e6e747f4d5f1b4080ed
Slava Pleshkov <mail@slavapleshkov.com>
2019-11-21 19:01:25 +0200

    Edit livmx

--- Libmx/main.c
+++
@@ -1,6 +0,0 @@
-#include "inc/libmx.h"
-
-int main() {
-    printf("%s", mx_file_to_str("hello.txt"));
-    return 0;
-}
--- Libmx/src/mx_file_to_str.c
+++ Libmx/src/mx_file_to_str.c
@@ -1,5 +1,40 @@
 #include "libmx.h"
 
+int mx_validation_file(const char *file)
+{
+    int fd = open(file, O_RDONLY);
+
+    if (fd < 0)
+    {
+        close(fd);
+        return 0;
+    }
+    else if (read(fd, (void *)0, 0) < 0)
+    {
+        close(fd);
+        return 0;
+    }
+    else if (!file)
+    {
+        close(fd);
+        return 0;
+    }
+    return 1;
+}
+
+static int mx_leng_file(const char *file)
+{
+    char buf;
+    int len = 0;
+    int fd = open(file, O_RDONLY);
+    while (read(fd, &buf, 1))
+    {
+        len++;
+    }
+    close(fd);
+    return len;
+}
+
 char *mx_file_to_str(const char *file)
 {
     char buf;
